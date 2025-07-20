b9732515975fb337cf515f957c24006fadcc5d6c
harshit-byte1 <dhingrah360@gmail.com>
2025-07-20 19:16:10 +0930

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -6,7 +6,7 @@ import powerbiLogo from './assets/tech_logo/powerbi.webp';
 import tableauLogo from './assets/tech_logo/tableau1.png';
 import jupyterLogo from './assets/tech_logo/jupyter.svg';
 import colabLogo from './assets/tech_logo/colab.png';
-import houseLogo from './assets/work_logo/chatGPT_IMAGE.png';
+import houseLogo from './assets/work_logo/ChatGPT_Image.png';
 
 import pythonLogo from './assets/tech_logo/python.png';
 import flaskLogo from './assets/tech_logo/flask.png';
