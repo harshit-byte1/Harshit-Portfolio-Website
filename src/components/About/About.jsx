f67ea3ef02c7bb619619093b7ccab111a67e22e0
harshit-byte1 <dhingrah360@gmail.com>
2025-07-20 19:29:25 +0930

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -64,25 +64,27 @@ const About = () => {
           </a>
           
         </div>
-        {/* Right Side */}
-        <div className="md:w-1/2 flex justify-center md:justify-end">
-          <Tilt
-            className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
-            tiltMaxAngleX={20}
-            tiltMaxAngleY={20}
-            perspective={1000}
-            scale={1.05}
-            transitionSpeed={1000}
-            gyroscope={true}
-          >
-            <img
-              src={profileImage}
-              alt="Harshit Dhingra"
-              className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
-            />
-          </Tilt>
-        </div>
-      </div>
+{/* Right Side */}
+<div className="md:w-1/2 flex justify-center md:justify-end">
+  <Tilt
+    className="w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 rounded-full"
+    tiltMaxAngleX={20}
+    tiltMaxAngleY={20}
+    perspective={1000}
+    scale={1.05}
+    transitionSpeed={1000}
+    gyroscope={true}
+  >
+    <div className="w-full h-full border-4 border-purple-700 rounded-full overflow-hidden shadow-xl">
+      <img
+        src={profileImage}
+        alt="Harshit Dhingra"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  </Tilt>
+</div>
+
     </section>
   );
 };
